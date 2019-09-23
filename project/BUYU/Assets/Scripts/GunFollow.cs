31267ce0477634c7d5eb384becd79bfc5d9c5d06
abu <macheng@hotmail.com>
2019-09-24 02:35:19 +0800

    画界面

---
+++ project/BUYU/Assets/Scripts/GunFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFollow : MonoBehaviour
+{
+    public RectTransform RectTransform;
+    public Camera Camera;
+
+    void Update()
+    {
+        Vector3 vector3;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(RectTransform, new Vector2
+            (
+            Input.mousePosition.x,
+            Input.mousePosition.y
+            ), Camera, out vector3);
+
+        var z = Vector3.Angle(Vector3.up, vector3 - transform.position) *
+            (
+                vector3.x > transform.position.x ? -1 : 1
+            );
+
+        transform.localRotation = Quaternion.Euler(0, 0, z);
+
+    }
+}
